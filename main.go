f7cff215b4f1da78b2413a84e3624387e5963b3d
arrobasaul <saul_v92@yahoo.es>
2019-02-22 18:33:30 -0600

    cambios de json

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+
+	ent "./entidades"
 )
 
 func main() {
 	fmt.Println("Contabilidad de la casa")
+	m := ent.Cuenta{
+		CodCuenta:    1,
+		NombreCuenta: "",
+		Valor:        1.2,
+		Estado:       1,
+	}
+	b, _ := json.Marshal(m)
+	println(b)
 }
