94a9d4a174df997c7c9a51a87d95557c6e1a5d27
arrobasaul <saul_v92@yahoo.es>
2019-03-05 18:22:44 -0600

    DTO GENERICO

---
+++ dto/dto.go
@@ -0,0 +1,112 @@
+package dto
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+)
+
+type todo interface{}
+
+func GetAll(a interface{}) (*string, error) {
+
+	elementos := reflect.ValueOf(a).Elem()
+	nombre := reflect.TypeOf(a).Elem().Name()
+
+	var query string
+	query = "select "
+	for j := 0; j < elementos.NumField(); j++ {
+		nombreAtributo := elementos.Type().Field(j).Name
+		query += fmt.Sprintf(", %s ", nombreAtributo)
+	}
+
+	query = strings.Replace(query, "select ,", "select ", -1)
+	query += fmt.Sprintf(" from %s ", nombre)
+	return &query, nil
+}
+
+func GetById(a interface{}, id int) (*string, error) {
+
+	elementos := reflect.ValueOf(a).Elem()
+	nombre := reflect.TypeOf(a).Elem().Name()
+	var Millave string
+	var query string
+	query = "select "
+	for j := 0; j < elementos.NumField(); j++ {
+
+		nombreAtributo := elementos.Type().Field(j).Name
+
+		llave := elementos.Type().Field(j).Tag.Get("llave")
+		if llave == "SI" {
+			Millave = nombreAtributo
+		}
+		query += fmt.Sprintf(", %s ", nombreAtributo)
+	}
+	query = strings.Replace(query, "select ,", "select ", -1)
+	query += fmt.Sprintf(" from %s where %s = %d", nombre, Millave, id)
+
+	return &query, nil
+}
+func InsertAll(a interface{}) (*string, error) {
+
+	elementos := reflect.ValueOf(a).Elem()
+	nombre := reflect.TypeOf(a).Elem().Name()
+
+	var query string
+	query = "insert into " + nombre + " set "
+	for j := 0; j < elementos.NumField(); j++ {
+		//tag := typeField.Tag
+		//tag.Get("tag_name")
+
+		nombreAtributo := elementos.Type().Field(j).Name
+
+		switch elementos.Field(j).Kind() {
+		case reflect.Int:
+			valor := elementos.Field(j).Int()
+			query += fmt.Sprintf(", %s = %v", nombreAtributo, valor)
+		case reflect.String:
+			valor := elementos.Field(j).String()
+			query += fmt.Sprintf(", %s = \"%v\"", nombreAtributo, valor)
+		default:
+
+		}
+	}
+	query = strings.Replace(query, "set ,", " set ", -1)
+	return &query, nil
+}
+func UpdateById(a interface{}, id int) (*string, error) {
+
+	elementos := reflect.ValueOf(a).Elem()
+	nombre := reflect.TypeOf(a).Elem().Name()
+	var Millave string
+	var query string
+	query = "update " + nombre + " set "
+	for j := 0; j < elementos.NumField(); j++ {
+		//tag := typeField.Tag
+		//tag.Get("tag_name")
+
+		nombreAtributo := elementos.Type().Field(j).Name
+		llave := elementos.Type().Field(j).Tag.Get("llave")
+		if llave == "SI" {
+			Millave = nombreAtributo
+			continue
+		}
+		switch elementos.Field(j).Kind() {
+		case reflect.Int:
+			valor := elementos.Field(j).Int()
+			if valor != 0 {
+				query += fmt.Sprintf(", %s = %v", nombreAtributo, valor)
+			}
+		case reflect.String:
+			valor := elementos.Field(j).String()
+			if valor != "" {
+				query += fmt.Sprintf(", %s = \"%v\"", nombreAtributo, valor)
+			}
+		default:
+
+		}
+	}
+	query = strings.Replace(query, "set ,", " set ", -1)
+	query += fmt.Sprintf(" where %s = %v", Millave, id)
+	return &query, nil
+}
--- entidades/usuarios.go
+++ entidades/usuarios.go
@@ -2,7 +2,7 @@ package entidades
 
 //Usuario todo los usuarios que conectaran con el sistema
 type Usuario struct {
-	CodUsuario    int    `json:"CodUsuario"`
+	CodUsuario    int    `json:"CodUsuario" llave:"SI"`
 	NombreUsuario string `json:"NombreUsuario"`
 	Correo        string `json:"Correo"`
 	Password      string `json:"Password"`
